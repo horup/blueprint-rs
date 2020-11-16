223ffd2324985258868eb1b35911ff0c97e66336
horup <sah@sah.dk>
2020-11-16 19:45:47 +0100

    added system support

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,7 +1,16 @@
-use blueprint::engine::Engine;
+use blueprint::{context::Context, engine::Engine};
 
+
+fn tick(ctx:&mut Context<(),(),()>) 
+{
+
+}
 
 fn main() {
     let mut engine:Engine<(),(),()> = Engine::new();
+    engine.systems_mut().push(|x| {
+    });
+
+    engine.systems_mut().push(tick);
     Engine::run(engine);
 }

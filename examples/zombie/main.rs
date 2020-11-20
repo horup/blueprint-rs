7cf1715e344231e5a69247ca9620957914484b12
horup <sah@sah.dk>
2020-11-20 21:23:28 +0100

    renamed simple to zombie

--- examples/zombie/main.rs
+++ examples/zombie/main.rs
@@ -1,37 +1,37 @@
 use blueprint::{context::Context, engine::Engine, math::Rect2, art::Art, world::GameWorld};
 use rand::random;
 
-struct SimpleWorld {
+struct ZombieWorld {
     pub timer:f32
 }
 
-impl GameWorld for SimpleWorld {
-    type Sprite = SimpleSprite;
-    type Event = SimpleEvent;
-    type Art = SimpleArt;
-    type Texture = SimpleTexture;
+impl GameWorld for ZombieWorld {
+    type Sprite = ZombieSprite;
+    type Event = ZombieEvent;
+    type Art = ZombieArt;
+    type Texture = ZombieTexture;
 }
 
 #[derive(Debug, Copy, Clone, Default)]
-struct SimpleSprite {
+struct ZombieSprite {
 }
 
 #[derive(Debug, Copy, Clone)]
-enum SimpleEvent {
+enum ZombieEvent {
 }
 
 #[derive(Copy, Clone, Hash, Eq, PartialEq)]
-enum SimpleArt {
+enum ZombieArt {
     Player,
     Zombie
 }
 
 #[derive(Copy, Clone, Hash, Eq, PartialEq)]
-enum SimpleTexture {
+enum ZombieTexture {
     Spritesheet
 }
 
-impl Default for SimpleWorld {
+impl Default for ZombieWorld {
     fn default() -> Self {
         Self {
             timer:0.0
@@ -39,7 +39,7 @@ impl Default for SimpleWorld {
     }
 }
 
-fn tick(ctx:&mut Context<SimpleWorld>) 
+fn tick(ctx:&mut Context<ZombieWorld>) 
 {
     match ctx.event {
         blueprint::event::Event::Tick(delta) => {
@@ -51,7 +51,7 @@ fn tick(ctx:&mut Context<SimpleWorld>)
                 
                 *timer = 0.0;
                             
-                let mut s = ctx.world.new_sprite(SimpleArt::Zombie);
+                let mut s = ctx.world.new_sprite(ZombieArt::Zombie);
                 let dy = 8.0;
                 let dx = 16.0;
                 s.pos.x = random::<f32>() * dx - dx / 2.0;
@@ -66,29 +66,29 @@ fn tick(ctx:&mut Context<SimpleWorld>)
 }
 
 fn main() {
-    let mut engine:Engine<SimpleWorld> = Engine::new();
+    let mut engine:Engine<ZombieWorld> = Engine::new();
     engine.camera.zoom = 32.0;
-    engine.load_texture(include_bytes!("spritesheet.png"), SimpleTexture::Spritesheet);
-    engine.art.insert(SimpleArt::Player, Art {
+    engine.load_texture(include_bytes!("spritesheet.png"), ZombieTexture::Spritesheet);
+    engine.art.insert(ZombieArt::Player, Art {
         animation : blueprint::art::Animation::LoopBackForth,
         frames:[Rect2::new(0.0, 0.0, 16.0, 16.0), Rect2::new(0.0, 16.0, 16.0, 16.0)].into(),
         frames_per_second:2.0,
         height:1.0,
         width:1.0,
-        texture_id:SimpleTexture::Spritesheet
+        texture_id:ZombieTexture::Spritesheet
     });
-    engine.art.insert(SimpleArt::Zombie,Art {
+    engine.art.insert(ZombieArt::Zombie,Art {
         animation : blueprint::art::Animation::LoopBackForth,
         frames:[Rect2::new(16.0, 0.0, 16.0, 16.0), Rect2::new(16.0, 16.0, 16.0, 16.0)].into(),
         frames_per_second:2.0,
         height:1.0,
         width:1.0,
-        texture_id:SimpleTexture::Spritesheet
+        texture_id:ZombieTexture::Spritesheet
     });
   
-    engine.config.window_title = "Simple Example".into();
+    engine.config.window_title = "Zombie Example".into();
 
-    let mut s = engine.world.new_sprite(SimpleArt::Player);
+    let mut s = engine.world.new_sprite(ZombieArt::Player);
     s.pos.x = 0.0;
     s.pos.y = 0.0;
 
